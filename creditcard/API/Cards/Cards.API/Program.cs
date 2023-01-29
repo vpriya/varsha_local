970669237fd6c7533da6df119c0c874fef39a31f
ONBASE\vpriya <varsha.priya@hyland.com>
2023-01-29 18:45:16 +0530

    credit card c# code working

--- creditcard/API/Cards/Cards.API/Program.cs
+++ creditcard/API/Cards/Cards.API/Program.cs
@@ -1,3 +1,6 @@
+using Cards.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject Dbcontext
+builder.Services.AddDbContext<CardsDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("CardsDbConnectionString")));
+
+
 
 var app = builder.Build();
 
