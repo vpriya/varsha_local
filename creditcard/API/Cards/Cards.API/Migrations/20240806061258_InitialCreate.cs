6dad3c3b5c3ede4c12dd9a2c870f4703dc937840
vpriya <varsha.priya@equifax.com>
2024-08-06 11:55:19 +0530

    changes in new laptop for recreating the DB

---
+++ creditcard/API/Cards/Cards.API/Migrations/20240806061258_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cards.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CardholderName = table.Column<string>(type: "varchar(60)", nullable: true),
+                    CardNumber = table.Column<string>(type: "varchar(16)", nullable: true),
+                    ExpiryMonth = table.Column<string>(type: "varchar(2)", nullable: false),
+                    ExpiryYear = table.Column<string>(type: "varchar(2)", nullable: false),
+                    CVC = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
